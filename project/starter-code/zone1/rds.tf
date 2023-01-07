928693eb1eaab8278d6fb4dbd3e9c8d1d70a3d33
ceblfe <ceblfe@gmail.com>
2023-01-07 22:22:36 +0100

    Step 6. Updating parameters in rds.tf

--- project/starter-code/zone1/rds.tf
+++ project/starter-code/zone1/rds.tf
@@ -2,6 +2,9 @@ module "project_rds_p" {
   source = "./modules/rds-p"
   private_subnet_ids = module.vpc.private_subnet_ids
   vpc_id = module.vpc.vpc_id
+  availability_zones = ["us-east-2a","us-east-2b","us-east-2c"]
+  rds_instances_count = 2
+  rds_backup_retention_period = 5
 }
 
 module "project_rds_s" {      #TODO descomentamos el modulo
@@ -12,4 +15,7 @@ module "project_rds_s" {      #TODO descomentamos el modulo
   }
   private_subnet_ids = module.vpc_west.private_subnet_ids
   vpc_id = module.vpc_west.vpc_id
+  availability_zones = ["us-west-1a","us-west-1c"]
+  rds_instances_count = 2
+  rds_backup_retention_period = 5
 }
