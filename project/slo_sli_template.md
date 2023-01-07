e1453af57a811987b8d558b59ea270074b4806cb
ceblfe <ceblfe@gmail.com>
2023-01-07 12:39:38 +0100

    Step 4 and Step 5 deploy and update zone1 and zone2 with alb. Update parameters

---
+++ project/slo_sli_template.md
@@ -0,0 +1,6 @@
+﻿| **Category**     | **SLI**                                                                                | **SLO**                                                                                                     |
+|------------------|----------------------------------------------------------------------------------------|-------------------------------------------------------------------------------------------------------------|
+| **Availability** | Total number of successful request / total number of requests                          | 99%                                                                                                         |
+| **Latency**      | 90% percentile of all requests complete in less 100 ms                                 | 90% of requests below 100 ms                                                                                |
+| **Error Budget** | Proportion of the all failed requests against the proportion all failed requests used  | Error budget is defined at 20%. This means that 20% of the requests can fail and still be within the budget |
+| **Throughput**   | Total number of successful requests by the time interval in seconds                    | 5 RPS indicates the application is functioning                                                              |
---
+++ project/starter-code/zone2/modules/alb/alb.tf
@@ -0,0 +1,54 @@
+# APLICATION LOAD BALANCER (alb)
+# Ref.: https://skundunotes.com/2022/07/30/add-an-application-load-balancer-to-aws-ec2-using-terraform/
+###########################
+# 1. Create a target group
+# Ref.: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lb_target_group
+###########################
+resource "aws_lb_target_group" "udacity" {
+  name     = "udacity-lb-alb-west-tg"
+  target_type = "alb"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+}
+
+#############################################
+# 2. Attach the target group to AWS instances
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lb_target_group_attachment
+#############################################
+resource "aws_lb_target_group_attachment" "udacity" {
+  count            = 2
+  target_group_arn = aws_lb_target_group.udacity.arn
+  target_id        = var.ec2.*.id[count.index]
+  port             = 80
+}
+
+#############################
+# 3. Create the load balancer
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lb
+#############################
+resource "aws_lb" "udacity" {
+  name               = "udacity-lb-alb-west-tf"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [var.ec2_sg]
+  subnets            = var.subnet_id
+
+  enable_deletion_protection = false
+
+}
+
+######################
+# 4. Create a listener
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lb_listener
+######################
+resource "aws_lb_listener" "udacity" {
+  load_balancer_arn = aws_lb.udacity.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.udacity.arn
+  }
+}
